ca731e60344e0db010f1e30dffbb96f99675127b
OME <Bizkitapidev@gmail.com>
2025-03-12 19:31:14 +0700

    update

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ import productRoute from "./routes/productRoute.js"
 import memberRoute from "./routes/memberRoute.js"
 import cartRoute from "./routes/cartRoute.js"
 import cors from "cors"
+// import ส่วนที่ติดตั้งเข้ามา
+import swaggerUI from "swagger-ui-express"
+import yaml from "yaml"
+// ใช้ File
+import fs from "fs"
+
 
 dotenv.config()
 
 const app=express()
 const port=process.env.PORT
+
 
 app.use(bodyParser.json())
 app.use(cors({
@@ -24,6 +31,12 @@ app.use("/img_mem",express.static("img_mem"))
 app.use(productRoute)
 app.use(memberRoute)
 app.use(cartRoute)
+
+// swagger
+const swaggerfile = fs.readFileSync('service/swagger.yaml','utf-8')
+const swaggerDoc = yaml.parse(swaggerfile)
+// กำหนด path ที่จะให้เรียกหน้า Document ขึ้นมา
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDoc))
 
 app.get('/',(req,res)=>{
     console.log(`GET / Requested`)
